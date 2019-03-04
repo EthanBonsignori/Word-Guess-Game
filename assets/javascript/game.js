7a9ffde2822a8c88285701a4f4432e9bd8898815
Ethan <ebonsignori@gmail.com>
2019-03-04 12:54:40 -0500

    cleaned up readme, minor changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,21 @@
 // Declare variables
-var hangman = {
+const hangman = {
       wins: 0,      // Number of times the user successfully guessed the word
       losses: 0,     // Number of times the user ran out of guesses
       guesses: 10,    // User's guesses remaining
       countries: ["united states of america", "japan", "canada", "new zealand", "russia", "sweden"],  // List of words to choose from for the user to guess
-      chosenWord: "",
-      alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",   
-                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],  // List of acceptable keys the user can guess
+      chosenWord: "", // Holds the randomly selected word from the randomWord function
+      alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",   // List of acceptable keys the user can guess
+                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],  
       usedLetters: [], // Holds users guessed letters so they cannot be re-used
       underscores: [], // Will populate screen with underscores to match the number of letters in chosen random country
 
       randomWord: function() {  // Picks a random word in the countries array and displays it to console
-      let randomNumber = Math.floor(Math.random() * this.countries.length);
-      this.chosenWord = this.countries[randomNumber];
-        for(let i = 0; i < this.chosenWord.length; i++) { // Adds one underscore per letter in the randomly selected word from the countries array
-          this.underscores.push('_');
-        }
+        let randomNumber = Math.floor(Math.random() * this.countries.length);
+        this.chosenWord = this.countries[randomNumber];
+          for(let i = 0; i < this.chosenWord.length; i++) { // Adds one underscore per letter in the randomly selected word from the countries array
+            this.underscores.push('_');
+          }
       },
 
       debug: function() { // Outputs necessary variables to console for debugging
